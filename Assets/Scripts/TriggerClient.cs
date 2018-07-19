af356db7a3c4aee3e40abd5ae0391bb34dc47c5b
nathanbaum <nathan.baum@nyu.edu>
2018-07-19 02:30:08 -0400

    got local network working on mobile, but still need to figure out network discovery

---
+++ Assets/Scripts/TriggerClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace nb2255
+{
+    public class TriggerClient : ITrigger
+    {
+        public NetworkDiscovery nd = null;
+        private callback closeMenu;
+
+        void Start()
+        {
+            if (nd == null)
+            {
+                nd = FindObjectOfType<NetworkDiscovery>();
+            }
+            nd.Initialize();
+        }
+
+        public override string GetName()
+        {
+            return "Join existing as Client";
+        }
+
+        public override void Toggle()
+        {
+            nd.StartAsClient();
+            closeMenu();
+        }
+
+        public override void setCallBacks(callback close, setter set)
+        {
+            closeMenu = close;
+        }
+
+    }
+}
---
+++ Assets/Scripts/TriggerServer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace nb2255
+{
+    public class TriggerServer : ITrigger
+    {
+        public NetworkDiscovery nd = null;
+        public NetworkManager nm = null;
+        private callback closeMenu;
+
+        void Start()
+        {
+            if( nd == null )
+            {
+                nd = FindObjectOfType<NetworkDiscovery>();
+            }
+            if (nm == null)
+            {
+                nm = FindObjectOfType<NetworkManager>();
+            }
+            nd.Initialize();
+        }
+
+        public override string GetName()
+        {
+            return "Start as Server";
+        }
+
+        public override void Toggle()
+        {
+            nm.StartHost();
+            nd.StartAsServer();
+            closeMenu();
+        }
+
+        public override void setCallBacks(callback close, setter set)
+        {
+            closeMenu = close;
+        }
+
+    }
+}
